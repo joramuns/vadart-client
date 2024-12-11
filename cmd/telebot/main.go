3f205de9658b0ca6a2a859cb141c55f310c5fb8e
Joramun Sasin <joramuns@student.21-school.ru>
2024-12-11 21:38:33 +0300

    feat: sleep, clearall, add, refresh commands

--- cmd/telebot/main.go
+++ cmd/telebot/main.go
@@ -8,10 +8,14 @@ import (
 	"gopkg.in/tucnak/telebot.v2"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	tele "gopkg.in/telebot.v4"
 )
+
+const owner = 89268804
 
 func SubscribeTrading(rdb *vadart_redis.Connection, b *tele.Bot) {
 	channelName := "articles"
@@ -26,9 +30,15 @@ func SubscribeTrading(rdb *vadart_redis.Connection, b *tele.Bot) {
 		case message := <-channel:
 			if message.Payload[:3] == "del" {
 				article := "Article bought:" + message.Payload[3:len(message.Payload)]
-				msg, err := b.Send(telebot.ChatID(89268804), article)
+				msg, err := b.Send(telebot.ChatID(owner), article)
 				if err != nil {
-					log.Fatal(err, "message:", msg)
+					log.Println(err, "message:", msg)
+				}
+			} else if message.Payload[:3] == "add" {
+				article := "Start hunting article" + message.Payload[3:len(message.Payload)]
+				msg, err := b.Send(telebot.ChatID(owner), article)
+				if err != nil {
+					log.Println(err, "message:", msg)
 				}
 			}
 		}
@@ -61,6 +71,68 @@ func main() {
 		}
 		return c.Send(message)
 	})
+
+	b.Handle("/refresh", func(c tele.Context) error {
+		err := rdb.RefreshPubSub()
+		if err != nil {
+			c.Send("Error refreshing:", err)
+		}
+		return nil
+	})
+
+	b.Handle("/clearall", func(c tele.Context) error {
+		err := rdb.ClearAll()
+		if err != nil {
+			log.Println("Error refreshing:", err)
+		}
+		return nil
+	})
+
+	b.Handle("/add", func(c tele.Context) error {
+		text := c.Message().Text
+		parts := strings.SplitN(text, " ", 4)
+
+		if len(parts) == 2 {
+			err = rdb.AddItem(parts[1], 0, 999999)
+			if err != nil {
+				return c.Send("Error in add item:", err)
+			}
+			return nil
+		} else if len(parts) < 4 {
+			return c.Send("Not enough arguments provided")
+		}
+
+		minPrice, err := strconv.Atoi(parts[2])
+		if err != nil {
+			return c.Send("Wrong min price argument")
+		}
+		maxPrice, err := strconv.Atoi(parts[3])
+		if err != nil {
+			return c.Send("Wrong max price argument")
+		}
+
+		err = rdb.AddItem(parts[1], minPrice, maxPrice)
+		if err != nil {
+			return c.Send("Error in add item:", err)
+		}
+		return nil
+	})
+
+	b.Handle("/sleep", func(c tele.Context) error {
+		text := c.Message().Text
+		parts := strings.SplitN(text, " ", 3)
+
+		if len(parts) < 3 {
+			return c.Send("Wrong sleep format - /sleep receiver seconds")
+		}
+
+		err := rdb.Command(parts[1], "sleep", parts[2])
+		if err != nil {
+			c.Send("Error in command sleep:", err)
+		}
+		return nil
+	})
+
 	go SubscribeTrading(rdb, b)
 
 	b.Start()
