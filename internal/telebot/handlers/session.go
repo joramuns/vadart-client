e1dcb015ffe8c408234acfd79e7e93cb1b6aaf44
Joramun Sasin <joramuns@student.21-school.ru>
2024-12-25 16:29:03 +0300

    fix: message parsing, send session id to redis

--- internal/telebot/handlers/session.go
+++ internal/telebot/handlers/session.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	vr "github.com/joramuns/vadart-client/pkg/vadart-redis"
 	tele "gopkg.in/telebot.v4"
 	"strings"
@@ -11,9 +12,10 @@ func Session(rdb *vr.Connection) tele.HandlerFunc {
 		text := c.Message().Text
 		parts := strings.SplitN(text, " ", 2)
 		if len(parts) < 2 {
-			return c.Send("Wrong session format - /session session_id")
+			return c.Send("Wrong session format - /session session-id")
 		}
 
+		rdb.Conn.Set(context.Background(), "PHPSESSID", parts[1], -1)
 		err := rdb.Command("ALL", "session", parts[1])
 		if err != nil {
 			return c.Send("Error in command check:", err)
